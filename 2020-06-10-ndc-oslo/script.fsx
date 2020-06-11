fa18e9743ab21d23b5e19acb5044e72e7811c8df
Don Syme <dsyme@microsoft.com>
2020-06-11 11:51:55 +0100

    new files

---
+++ 2020-06-10-ndc-oslo/script.fsx
@@ -0,0 +1,83 @@
+#r @"C:\GitHub\dsyme\FSharp.Data.Adaptive\bin\Release\netstandard2.0\FSharp.Data.Adaptive.dll"
+
+open FSharp.Data.Adaptive
+module CSet =
+    let ofList xs =
+       let t = new cset<_>()
+       for x in xs do t.Add x |> ignore
+       t
+
+
+//-----------------------------------------------------------------
+
+module Functional = 
+    // Make a large data struture
+    let c7 = [ 1 .. 10000 ]
+
+    // Transform the data structure
+    let s7a = c7 |> List.map (fun x -> printfn "map (1), x = %d" x; x * 10) 
+
+    // Transform again
+    let s7b = s7a |> List.map (fun x -> printfn "map (2), x = %d" x; x * 10) 
+
+    // Reduce the data structure
+    let s7 = List.sum s7b 
+
+    // Observe the reduced result
+    s7 
+
+    let compute (c7: int list) = 
+     
+        // Transform the data structure
+        let s7a = c7 |> List.map (fun x -> printfn "map (1), x = %d" x; x * 10) 
+
+        // Transform again
+        let s7b = s7a |> List.map (fun x -> printfn "map (2), x = %d" x; x * 10) 
+
+        // Reduce the data structure
+        let s7 = List.sum s7b 
+
+        // Observe the reduced result
+        s7 
+
+    compute [ 1 .. 10000 ]
+    compute ([ 1 .. 10000 ] |> List.filter (fun x -> x <> 5))
+
+//-----------------------------------------------------------------
+
+// Make a large data struture
+let c7 = CSet.ofList [ 1 .. 10000 ]
+
+// Transform the data structure
+let s7a = c7 |> ASet.map (fun x -> printfn "map (1), x = %d" x; x * 10) 
+
+// Transform again
+let s7b = s7a |> ASet.map (fun x -> printfn "map (2), x = %d" x; x * 10) 
+
+// Reduce the data structure
+let s7 = ASet.sum s7b 
+
+// Observe the reduced result
+s7 |> AVal.force
+
+// Transactionally modify the inputs
+transact (fun () -> c7.Remove 5) 
+
+// Re-observe
+s7 |> AVal.force
+
+// Transactionally modify the inputs
+transact (fun () -> c7.Add 2000000) 
+
+// Re-observe
+s7 |> AVal.force
+
+
+// Transactionally modify the inputs
+transact (fun () -> c7.Clear()) 
+
+// Re-observe
+s7 |> AVal.force
+
+
+
